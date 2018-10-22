e04e5d3903f7307e2508d9ac8cf29233b97272a4
Matt Thorstensen <mthorstensen@ucdavis.edu>
2018-10-22 13:31:57 -0500

    Update salmon_index.sh

--- shell_scripts/salmon_index.sh
+++ shell_scripts/salmon_index.sh
@@ -5,5 +5,6 @@
 
 module load nixpkgs/16.09  gcc/5.4.0  openmpi/2.1.1
 module load salmon/0.9.1
-
+# Load up the salmon program and all its dependancies, then run salmon in indexing mode on a transcriptome
+# This will give us an index directory where salmon will refer to when counting up transcript reads
 salmon index -t /home/biomatt/projects/def-kmj477/biomatt/Transcriptome/walleye_transcriptome.fa -i walleye_index --type quasi -k 31
