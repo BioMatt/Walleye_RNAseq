2549a0c889aaf6bf922302ebc42d6980441b01d4
Matt Thorstensen <mthorstensen@ucdavis.edu>
2018-11-03 11:43:12 -0500

    Update bowtie_index.sh

--- bowtie/bowtie_index.sh
+++ bowtie/bowtie_index.sh
@@ -5,8 +5,14 @@
 #SBATCH --mail-user=thorstem@myumanitoba.ca
 #SBATCH --mail-type=ALL
 
+# Set up slurm options and ask the cluster to email me when the program starts, stops, or errors out.
+
+# All of these modules are required for bowtie2 on this cluster. They're just the ones pulled from the command
+# module spider bowtie2/2.3.4.1 on the command line.
 module load nixpkgs/16.09  intel/2016.4
 
 module load bowtie2/2.3.4.1
 
+# Build the actual bowtie index, using "walleye" as the prefix for the various index files. They get created 
+# at the same directory the script is.
 bowtie2-build /home/biomatt/projects/def-kmj477/biomatt/Transcriptome/walleye_transcriptome.fa walleye
